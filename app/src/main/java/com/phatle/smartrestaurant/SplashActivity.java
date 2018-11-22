decacd5bf86e82a3768694a65d3506481fadeaa4
Le Khac Truong Phat <lekhactruongphat347@gmail.com>
2018-11-22 11:32:47 +0700

    Splash screen

---
+++ app/src/main/java/com/phatle/smartrestaurant/SplashActivity.java
@@ -0,0 +1,84 @@
+package com.phatle.smartrestaurant;
+
+import android.animation.Animator;
+import android.animation.ObjectAnimator;
+import android.content.Context;
+import android.content.Intent;
+import android.graphics.Typeface;
+import android.net.ConnectivityManager;
+import android.net.NetworkInfo;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.ProgressBar;
+import android.widget.TextView;
+
+public class SplashActivity extends AppCompatActivity {
+
+    private ProgressBar mProgressBar;
+    TextView appName;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+        mProgressBar = (ProgressBar) findViewById(R.id.progressBar2);
+        appName = findViewById(R.id.app_name);
+        Typeface face = Typeface.createFromAsset(getAssets(),
+                "fonts/open_sans_regular.ttf");
+        appName.setTypeface(face);
+        startAnimation();
+    }
+    private void startAnimation() {
+        ObjectAnimator progressAnimator = ObjectAnimator.ofInt(mProgressBar, "progress", 0, 100);
+        progressAnimator.setDuration(3000)
+                .addListener(new Animator.AnimatorListener() {
+                    @Override
+                    public void onAnimationStart(Animator animator) {
+                    }
+
+                    @Override
+                    public void onAnimationEnd(Animator animator) {
+                        checkNetWork();
+                    }
+
+                    @Override
+                    public void onAnimationCancel(Animator animator) {
+                    }
+
+                    @Override
+                    public void onAnimationRepeat(Animator animator) {
+                    }
+                });
+        progressAnimator.start();
+    }
+    public void checkNetWork()
+    {
+
+        if (!isOnline(this))
+        {
+            showNetworkAlert(this, new View.OnClickListener() {
+                @Override
+                public void onClick(View view) {
+                    finish();
+                    startActivity(getIntent());
+                }
+            });
+        }
+        else {
+            Intent intent = new Intent(SplashActivity.this,MainActivity.class);
+            startActivity(intent);
+            finish();
+        }
+    }
+    public static boolean isOnline(Context context) {
+        ConnectivityManager cm = (ConnectivityManager) context.getSystemService(Context.CONNECTIVITY_SERVICE);
+        NetworkInfo netInfo = cm.getActiveNetworkInfo();
+        return netInfo != null && netInfo.isConnectedOrConnecting();
+    }
+    public static void showNetworkAlert(Context context, View.OnClickListener tryAgainClickListener) {
+        ErrorDialog dialog = new ErrorDialog(context, "Bạn đang ngoại tuyến!", "Vui lòng kiểm tra kết nối Internet của bạn và thử lại");
+        dialog.setupOkButton("Thử lại", tryAgainClickListener);
+        dialog.show();
+        dialog.setCanceledOnTouchOutside(false);
+    }
+}
